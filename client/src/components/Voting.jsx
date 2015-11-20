006e77263e200b2b87ca0c691d244ac000e6694b
Supasate Choochaisri <supasate.c@gmail.com>
2015-11-20 14:39:59 +0700

    Handle voted state and winner state

--- client/src/components/Voting.jsx
+++ client/src/components/Voting.jsx
@@ -4,12 +4,24 @@ export default React.createClass({
     getPair: function () {
         return this.props.pair || [];
     },
+    isDisbled: function () {
+        return !!this.props.hasVoted;
+    },
+    hasVotedFor: function (entry) {
+        return this.props.hasVoted === entry;
+    },
     render: function () {
         return <div className="voting">
-            {this.getPair().map(entry =>
-                <button key={entry}>
-                    <h1>{entry}</h1>
-                </button>
+            {this.props.winner ?
+                <div ref="winner">Winner is {this.props.winner}!</div> :
+                this.getPair().map(entry =>
+                    <button key={entry}
+                            disabled="{this.isDisbled()}"
+                            onClick={() => this.props.vote(entry)}>
+                        <h1>{entry}</h1>
+                        {this.hasVotedFor(entry) ?
+                            <div className="label">Voted</div> : null}
+                    </button>
             )}
         </div>;
     }
