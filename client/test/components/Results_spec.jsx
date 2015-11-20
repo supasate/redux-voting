ced688babec06d825a6f1be25ec78edc1e62ee99
Supasate Choochaisri <supasate.c@gmail.com>
2015-11-20 23:35:06 +0700

    Show winner on Results

--- client/test/components/Results_spec.jsx
+++ client/test/components/Results_spec.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import {List, Map} from 'immutable';
 import Results from '../../src/components/Results';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithClass} = TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithClass, Simulate} = TestUtils;
 
 describe("Results", () => {
     it("renders entries with vote counts or zero", () => {
@@ -21,5 +22,31 @@ describe("Results", () => {
         expect(train).to.contain('5');
         expect(days).to.contain('28 Days Later');
         expect(days).to.contain('0');
+    });
+
+    it("invokes the next callback when next button is clicked", () => {
+        let nextInvoked = false;
+        const next = () => nextInvoked = true;
+
+        const pair = List.of('Trainspotting', '28 Days Later');
+        const component = renderIntoDocument(
+            <Results pair={pair}
+                     tally={Map()}
+                     next={next}/>
+        );
+        Simulate.click(ReactDom.findDOMNode(component.refs.next));
+
+        expect(nextInvoked).to.equal(true);
+    });
+
+    it("renders the winner when there is one", () => {
+        const component = renderIntoDocument(
+            <Results winner="Trainspotting"
+                     pair={["Trainspotting", "28 Days Later"]}
+                     tally={Map()} />
+        );
+        const winner = ReactDom.findDOMNode(component.refs.winner);
+        expect(winner).to.be.ok;
+        expect(winner.textContent).to.contain('Trainspotting');
     });
 });
