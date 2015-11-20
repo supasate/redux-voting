b057d0e84369a363e3773fbbf73211feecb824da
Supasate Choochaisri <supasate.c@gmail.com>
2015-11-20 15:43:24 +0700

    Seperate Winner and Vote components

--- client/src/components/Voting.jsx
+++ client/src/components/Voting.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-    getPair: function () {
-        return this.props.pair || [];
-    },
-    isDisbled: function () {
-        return !!this.props.hasVoted;
-    },
-    hasVotedFor: function (entry) {
-        return this.props.hasVoted === entry;
-    },
     render: function () {
-        return <div className="voting">
+        return <div>
             {this.props.winner ?
-                <div ref="winner">Winner is {this.props.winner}!</div> :
-                this.getPair().map(entry =>
-                    <button key={entry}
-                            disabled="{this.isDisbled()}"
-                            onClick={() => this.props.vote(entry)}>
-                        <h1>{entry}</h1>
-                        {this.hasVotedFor(entry) ?
-                            <div className="label">Voted</div> : null}
-                    </button>
-            )}
+                <Winner ref="winner" winner={this.props.winner} /> :
+                <Vote {...this.props} />}
         </div>;
     }
 });
--- client/test/components/Voting_spec.js
+++ client/test/components/Voting_spec.js
@@ -19,7 +19,10 @@ describe("Voting", () => {
 
     it("invokes callback when a button is clicked", () => {
         let votedWith;
-        const vote = (entry) => votedWith = entry;
+        const vote = (entry) => {
+            console.log('on vote');
+            votedWith = entry;
+        };
 
         const component = renderIntoDocument(
             <Voting pair={["Trainspotting", "28 Days Later"]}
@@ -29,5 +32,17 @@ describe("Voting", () => {
         Simulate.click(buttons[0]);
 
         expect(votedWith).to.equal("Trainspotting");
+    });
+
+    it("disables buttons when user has voted", () => {
+        const component = renderIntoDocument(
+            <Voting pair={["Trainspotting", "28 Days Later"]}
+                    hasVoted="Trainspotting" />
+        );
+        const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+        expect(buttons.length).to.equal(2);
+        expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+        expect(buttons[1].hasAttribute('disabled')).to.equal(true);
     });
 });
