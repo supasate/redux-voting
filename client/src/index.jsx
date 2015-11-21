2e70b8bceabb29597ec12ddd1c56ac5f5c90a12b
Supasate Choochaisri <supasate.c@gmail.com>
2015-11-21 17:21:16 +0700

    Add redux devtools

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
-import {createStore, applyMiddleware} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {persistState} from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DevTools from './containers/DevTools';
 import io from 'socket.io-client';
 import reducer from './reducer';
 import {setState} from './action_creators';
@@ -12,14 +15,17 @@ import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+
+const finalCreateStore = compose(
+    applyMiddleware(remoteActionMiddleware(socket)),
+    DevTools.instrument(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+const store = finalCreateStore(reducer);
 socket.on('state', state =>
     store.dispatch(setState(state))
 );
-
-const createStoreWithMiddleware = applyMiddleware(
-    remoteActionMiddleware(socket)
-)(createStore);
-const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer} />
@@ -28,7 +34,10 @@ const routes = <Route component={App}>
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>{routes}</Router>
+        <div>
+            <Router>{routes}</Router>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('app')
 );
